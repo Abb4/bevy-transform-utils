4593310393b6b5932ff45b3f2d1593ed58b7efd0
Abb4 <abb.coding@proton.me>
2022-09-25 09:54:00 +0200

    added min distance support, added some docs

--- src/lib.rs
+++ src/lib.rs
@@ -3,10 +3,28 @@ use bevy::{
     time::Time,
 };
 
-pub fn move_towards(transform: &mut Transform, target: Vec3, step: f32, time: &Res<Time>) {
+/// Moves a given `Transform` towards a `target` `Vec3` by the distance of `step` until `target_min_distance` is reached.
+/// Automatically adjusts for `Time`.
+pub fn move_towards(
+    transform: &mut Transform,
+    target: Vec3,
+    step: f32,
+    time: &Res<Time>,
+    target_min_distance: f32,
+) {
     let distance = transform.translation.distance(target);
+
+    let mut travel_distance = step;
+
+    if distance <= target_min_distance {
+        return;
+    }
+
+    if distance <= target_min_distance + step {
+        travel_distance = distance - target_min_distance;
+    }
 
     transform.translation = transform
         .translation
-        .lerp(target, (step / distance) * time.delta_seconds());
+        .lerp(target, (travel_distance / distance) * time.delta_seconds());
 }
