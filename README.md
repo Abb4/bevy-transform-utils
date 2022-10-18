e3b47db1141a4cf2b8116862cb1987ff59a8128d
Abb4 <abb.coding@proton.me>
2022-10-18 15:10:06 +0200

    split up "glep-pure" functions from bevy dependent ones

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
 # bevy-transform-utils
-Collection of useful util functions for bevy transforms
+Collection of useful util functions for bevy transforms.
+
+## Compiling without bevy
+Bevy uses glam for its core types and the compilation time can be shortened significantly if bevy imports are avoided.
+If you don't need bevy, include/compile this crate with the `nobevytypes` [feature](https://doc.rust-lang.org/cargo/reference/features.html) by running `cargo build --features 'nobevytypes'`.
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,13 @@
+#[cfg(not(nobevytypes))]
 use bevy::{
-    prelude::{Res, Transform, Vec3, Vec2},
+    prelude::{Quat, Res, Transform, Vec2, Vec3},
     time::Time,
 };
 
+#[cfg(nobevytypes)]
+use glam::{Vec2, Vec3, Quat};
+
+#[cfg(not(nobevytypes))]
 /// Moves a given `Transform` towards a `target` `Vec3` by the distance of `step` until `target_min_distance` is reached.
 /// Automatically adjusts for `Time`.
 /// ## Returns
@@ -13,42 +18,67 @@ pub fn move_towards(
     step: f32,
     time: &Res<Time>,
     target_min_distance: f32,
-) -> Option<f32> {
-    let distance = transform.translation.distance(target);
-
-    if distance < target_min_distance {
-        return None;
-    }
-
-    transform.translation = transform
-        .translation
-        .lerp(target, step / distance * time.delta_seconds());
-
-    Some(distance)
+) {
+    transform.translation = move_towards_glam(
+        transform.translation,
+        target,
+        step,
+        time.delta_seconds(),
+        target_min_distance,
+    );
 }
 
+pub fn move_towards_glam(
+    translation: Vec3,
+    target: Vec3,
+    step: f32,
+    time_delta_sec: f32,
+    target_min_distance: f32,
+) -> Vec3 {
+    let distance = translation.distance(target);
+
+    if distance < target_min_distance {
+        return translation;
+    }
+
+    translation.lerp(target, step / distance * time_delta_sec)
+}
+
+#[cfg(not(nobevytypes))]
 // Given a `Transform` return the angle to rotate the ransform towards the `target`. See `transform.rotate_z`.
 pub fn get_angle_from_transform(transform: &Transform, target: &Vec2) -> f32 {
-    let vector_angle = f32::atan2(
-        transform.translation.y - target.y,
-        transform.translation.x - target.x,
-    );
+    get_angle_from_transform_glam(&transform.translation, &transform.rotation, target)
+}
 
-    let (transform_axis, mut transform_angle) = transform.rotation.to_axis_angle();
+pub fn get_angle_from_transform_glam(translation: &Vec3, rotation: &Quat, target: &Vec2) -> f32 {
+    let vector_angle = f32::atan2(translation.y - target.y, translation.x - target.x);
+
+    let (transform_axis, mut transform_angle) = rotation.to_axis_angle();
 
     transform_angle *= transform_axis.z;
 
     vector_angle - transform_angle
 }
 
+#[cfg(not(nobevytypes))]
 // Move `transform` by `distance` forwards, as described by `transform.rotation`.
 // Adjusted by `time`.
 pub fn move_forward_by_transform_rotation(mut transform: Transform, distance: f32, time: Time) {
-    let (transform_axis, mut transform_angle) = transform.rotation.to_axis_angle();
+    transform.translation += move_forward_by_transform_rotation_glam(transform.rotation, distance, time.delta_seconds());
+}
+
+pub fn move_forward_by_transform_rotation_glam(
+    rotation: Quat,
+    distance: f32,
+    time_delta_sec: f32,
+) -> Vec3 {
+    let (transform_axis, mut transform_angle) = rotation.to_axis_angle();
 
     transform_angle *= transform_axis.z;
 
-    // FIXME projectile speed better inside the component
-    transform.translation.x -= transform_angle.cos() * distance * time.delta_seconds();
-    transform.translation.y -= transform_angle.sin() * distance * time.delta_seconds();
+    Vec3::new(
+        -transform_angle.cos() * distance * time_delta_sec,
+        -transform_angle.sin() * distance * time_delta_sec,
+        0.0,
+    )
 }
